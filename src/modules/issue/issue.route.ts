45fb70fb88478b045993e539f9913a5d7c2fd4a0
Rocky Chowdhury <rocky20809@gmail.com>
2026-06-13 11:44:35 +0600

    auth middleware set to call in post issue api

--- src/modules/issue/issue.route.ts
+++ src/modules/issue/issue.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { issueController } from "./issue.controller";
+import auth from "../../middleware/auth";
 
 
 const router = Router();
 
-router.post("/",issueController.createIssue);
+router.post("/",auth(), issueController.createIssue);
 // router.get("");
 // router.get("");
 // router.patch("");
